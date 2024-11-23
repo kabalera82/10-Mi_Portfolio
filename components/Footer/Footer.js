aa4865a27e4241c26024271a9e1d05071d07477d
kabalera82 <kabalera@gmail.com>
2024-11-23 23:12:18 +0100

    imagenes

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,10 +3,10 @@ import "./Footer.css";
 export const Footer = () => `
   <h2>"Contáctame para feedback o cualquier cosa"</h2>
   <div class="footer-buttons">
-    ${Button("/assets/gmail.png", "mailto:kabalera@gmail.com", "Gmail")}
-    ${Button("/assets/github.png", "https://github.com/kabalera82", "GitHub")}
-    ${Button("/assets/linked-in.png", "https://www.linkedin.com/in/marcos-padilla-puentes-373761127/", "LinkedIn")}
-    ${Button("/assets/telegram-plane.png", "https://t.me/kabalera82", "Telegram")}
+    ${Button("/gmail.png", "mailto:kabalera@gmail.com", "Gmail")}
+    ${Button("/github.png", "https://github.com/kabalera82", "GitHub")}
+    ${Button("/linked-in.png", "https://www.linkedin.com/in/marcos-padilla-puentes-373761127/", "LinkedIn")}
+    ${Button("/telegram-plane.png", "https://t.me/kabalera82", "Telegram")}
   </div>
 `;
 export const Button = (iconPath, link, altText = "") => `
